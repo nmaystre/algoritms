3e6fc0db4b2a503118565e8dfc274e3dfb022095
Natalia <n.d.maistrenko@gmail.com>
2018-10-27 15:33:58 +0300

    сортировка выбором

--- 3/script.js
+++ 3/script.js
@@ -1,18 +1,18 @@
 const selectionSort = (arr) => {
 	let minValueI;
 	const arrLength = arr.length;
+
 	for (let i = 0; i < arrLength - 1; i++) {
 		minValueI = i;
 		for (let j = i + 1; j < arrLength; j++) {
 			if (arr[minValueI] > arr[j]) {
-				minValueI = j;
+				minValueI = arr[j];
 			}
+			[ arr[i], arr[minValueI] ] = [ arr[minValueI], arr[i] ];
 		}
-		[ arr[i], arr[minValueI] ] = [ arr[minValueI], arr[i] ];
 	}
 	return arr;
 };
 
 console.log(selectionSort([ 9, 1, 3, 0, 29 ]));
-console.log(selectionSort([ 1, 5, 12, -22, 45, 5, 0 ]));
-console.log(selectionSort([ 1, 5, 0 ]));
+console.log(selectionSort([ 1, -3, 4, 7, 22, 11, 45, -5, 0 ]));
