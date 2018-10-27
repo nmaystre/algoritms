8454084e378903ff0efa5d1520dfdc222cf3ef1d
Natalia <n.d.maistrenko@gmail.com>
2018-10-27 14:29:05 +0300

    bubble

---
+++ 1/script.js
@@ -0,0 +1,12 @@
+const bubbleSort = (arr) => {
+	for (let i = 0, lastI = arr.length - 1; i < lastI; i++) {
+		for (let j = 0, lastJ = lastI - i; j < lastJ; j++) {
+			if (arr[j] > arr[j + 1]) {
+				[ arr[j], arr[j + 1] ] = [ arr[j + 1], arr[j] ];
+			}
+		}
+	}
+	return arr;
+};
+
+console.log(bubbleSort([ 1, -3, 4, 7, 22, 11, 45, -5, 0 ]));
